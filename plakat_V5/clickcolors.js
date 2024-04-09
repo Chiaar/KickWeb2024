c3f91346091b4ae752e72232ace7e09f017170f4
Chiaar <chiara.tremml@stud.hslu.ch>
2024-04-09 22:16:48 +0200

    update

--- plakat_V5/clickcolors.js
+++ plakat_V5/clickcolors.js
@@ -1,44 +1,43 @@
-// Funktion zum Ändern der Farbe eines SVG-Elements
+// Farbänderung von Dots
 function changeColor(elementId) {
     var element = document.getElementById(elementId); // Das SVG-Element mit der entsprechenden ID wird ausgewählt
     if (element) {
         var circle = element.querySelector('circle');
         var currentColor = circle.getAttribute('fill'); // Aktuelle Farbe des Kreises abrufen
-        // Wenn die aktuelle Farbe schwarz ist, ändere sie in #ce5c16, sonst ändere sie in Schwarz
         var newColor = (currentColor === 'black') ? '#ce5c16' : 'black';
-        circle.setAttribute('fill', newColor); // Die Farbe des Kreises im SVG ändern
+        circle.setAttribute('fill', newColor); // Farbe der dots wird geändert
     }
 }
 
-// Klickereignisse für das SVG hinzufügen
-document.getElementById('gcircle').addEventListener('click', function() {
-    changeColor('gcircle'); // Ändert die Farbe des Kreises im gcircle-SVG
+// Klickereignisse für jeden einzelnen SVG Dot hinzugefügt
+document.getElementById('gcircle').addEventListener('click', function () {
+    changeColor('gcircle'); 
 });
-document.getElementById('ucircle').addEventListener('click', function() {
-    changeColor('ucircle'); // Ändert die Farbe des Kreises im gcircle-SVG
+document.getElementById('ucircle').addEventListener('click', function () {
+    changeColor('ucircle'); 
 });
-document.getElementById('tcircle').addEventListener('click', function() {
-    changeColor('tcircle'); // Ändert die Farbe des Kreises im gcircle-SVG
+document.getElementById('tcircle').addEventListener('click', function () {
+    changeColor('tcircle'); 
 });
-document.getElementById('e2circle').addEventListener('click', function() {
-    changeColor('e2circle'); // Ändert die Farbe des Kreises im gcircle-SVG
+document.getElementById('e2circle').addEventListener('click', function () {
+    changeColor('e2circle'); 
 });
-document.getElementById('fcircle').addEventListener('click', function() {
-    changeColor('fcircle'); // Ändert die Farbe des Kreises im gcircle-SVG
+document.getElementById('fcircle').addEventListener('click', function () {
+    changeColor('fcircle'); 
 });
-document.getElementById('ocircle').addEventListener('click', function() {
-    changeColor('ocircle'); // Ändert die Farbe des Kreises im gcircle-SVG
+document.getElementById('ocircle').addEventListener('click', function () {
+    changeColor('ocircle'); 
 });
-document.getElementById('rcircle').addEventListener('click', function() {
-    changeColor('rcircle'); // Ändert die Farbe des Kreises im gcircle-SVG
+document.getElementById('rcircle').addEventListener('click', function () {
+    changeColor('rcircle'); 
 });
-document.getElementById('m2circle').addEventListener('click', function() {
-    changeColor('m2circle'); // Ändert die Farbe des Kreises im gcircle-SVG
+document.getElementById('m2circle').addEventListener('click', function () {
+    changeColor('m2circle'); 
 });
 
 
- // Update the position of the custom cursor based on mouse movement
- document.addEventListener('mousemove', function(event) {
+// Update the position of the custom cursor based on mouse movement
+document.addEventListener('mousemove', function (event) {
     var cursor = document.querySelector('.custom-cursor');
     cursor.style.left = event.clientX + 'px';
     cursor.style.top = event.clientY + 'px';
